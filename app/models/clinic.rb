c580cca9b08d962363fbc8efbcca315a38b58a75
charinrat bansong <charinrat_b@hotmail.com>
2020-11-30 17:15:04 +0800

    add geocoded_by :address to clinic model

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -3,4 +3,7 @@ class Clinic < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
