d7f59b26a024e66e029f18fbb5d0f5e516e1a679
charinrat bansong <charinrat_b@hotmail.com>
2020-12-01 15:41:05 +0800

    move map from clinic page to user dashbord

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,15 +1,8 @@
 class ClinicsController < ApplicationController
   before_action :set_clinic
     def index
-      @clinics = Clinic.all
-      @markers = @clinics.geocoded.map do |clinic|
-        {
-          lat: clinic.latitude,
-          lng: clinic.longitude,
-          infoWindow: render_to_string(partial: "info_window", locals: { clinic: clinic }),
-          #image_url: :CLOUDINARY_URL
-          }
-      end
+  
+    
     end
 
     def new
